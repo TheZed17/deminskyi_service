f41a31f0f0bf453ec8286fef4e2440dc0cc304db
Igor Deminski <kartoschka17gmail.com>
2023-05-23 07:28:36 +0300

    #4 [add] annotate and aggregate orm

--- service/services/serializers.py
+++ service/services/serializers.py
@@ -10,10 +10,14 @@ class PlanSerializer(serializers.ModelSerializer):
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
-    plan = PlanSerializer
+    plan = PlanSerializer()
     client_name = serializers.CharField(source='client.company_name')
     email = serializers.CharField(source='client.user.email')
+    price = serializers.SerializerMethodField()
+
+    def get_price(self, instance):
+        return instance.price
 
     class Meta:
         model = Subscription
-        fields = ('id', 'plan_id', 'client_name', 'email', 'plan')
+        fields = ('id', 'plan_id', 'client_name', 'email', 'plan', 'price')
